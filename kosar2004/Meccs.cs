48515ee323de0d50ee5360931f09aeb3ec7082e8
SzklevanMilan <szklevanm@gmail.com>
2020-10-15 10:14:15 +0200

    Órai munka 10.15 befejezve

--- kosar2004/Meccs.cs
+++ kosar2004/Meccs.cs
@@ -1,4 +1,6 @@
-﻿namespace kosar2004
+﻿using System.Collections.Generic;
+
+namespace kosar2004
 {
     internal class Meccs
     {
@@ -8,14 +10,21 @@
         public int IPont { get; private set; }
         public string Hely { get; private set; }
         public string Ido { get; private set; }
-        public Meccs(string hazai,string idegen,int hpont,int ipont,string hely,string ido)
+        public Meccs(/*string hazai,string idegen,int hpont,int ipont,string hely,string ido*/string sor)
         {
-            this.Hazai = hazai;
-            this.Idegen = idegen;
-            this.HPont = hpont;
-            this.IPont = ipont;
-            this.Hely = hely;
-            this.Ido = ido;
+            string[] adat = sor.Split(';');
+            Hazai = adat[0];
+            Idegen = adat[1];
+            HPont = int.Parse(adat[2]);
+            IPont = int.Parse(adat[3]);
+            Hely = adat[4];
+            Ido = adat[5];
+            //this.Hazai = hazai;
+            //this.Idegen = idegen;
+            //this.HPont = hpont;
+            //this.IPont = ipont;
+            //this.Hely = hely;
+            //this.Ido = ido;
 
         }
     }
--- kosar2004/Program.cs
+++ kosar2004/Program.cs
@@ -4,15 +4,124 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.ComponentModel;
+using System.ComponentModel.Design;
 
 namespace kosar2004
 {
     class Program
     {
+        static List<Meccs> meccsek = new List<Meccs>();
+        static Dictionary<string, int> stadion = new Dictionary<string, int>();
+        static void MasodikFeladat()
+        {
+            StreamReader sr = new StreamReader("eredmenyek.csv");
+            sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                meccsek.Add(new Meccs(sr.ReadLine()));
+            }
+            sr.Close();
+        }
+
+        static void HarmadikFeladat()
+        {
+            int haz = 0;
+            int idegen = 0;
+            foreach (var m in meccsek)
+            {
+                if (m.Hazai == "Real Madrid")
+                {
+                    haz++;
+                }
+                if (m.Idegen == "Real Madrid")
+                {
+                    idegen++;
+                }
+            }
+            Console.Write("3.feladat:Real Madrid: Hazai: {0}, Idegen: {1}", haz,idegen);
+
+        }
+
+        static void NegyedikFeladat()
+        {
+            bool dont = false;
+            foreach (var m in meccsek)
+            {
+                if (m.HPont == m.IPont)
+                {
+                    dont = true;
+                }
+                else
+                {
+                    dont = false;
+                }
+            }
+            if (dont == true)
+            {
+                Console.Write("\n4.feladat:Volt döntetlen? Igen");
+            }
+            else
+            {
+                Console.Write("\n4.feladat:Volt döntetlen? Nem");
+            }
+        }
+
+        static void OtodikFeladat()
+        {
+            string nev = "";
+            foreach (var m in meccsek)
+            {
+                if (m.Hazai.Contains("Barcelona"))
+                {
+                    nev = m.Hazai;
+                }
+            }
+            Console.WriteLine("\n5.feladat:barcelonai csapat neve: {0}",nev);
+        }
+
+        static void HatodikFeladat()
+        {
+            var datum = from m in meccsek
+                        where m.Ido == "2004-11-21"
+                        select m;
+            foreach (var d in datum)
+            {
+                Console.WriteLine($"{d.Hazai} - {d.Idegen} ({d.HPont}:{d.IPont})");
+            }
+        }
+
+        static void HetedikFeladat()
+        {
+            foreach (var m in meccsek)
+            {
+                if (!stadion.ContainsKey(m.Hely))
+                {
+                    stadion.Add(m.Hely,0);
+                }
+            }
+            foreach (var i in meccsek)
+            {
+                stadion[i.Hely]++;
+            }
+            foreach (var i in stadion)
+            {
+                if (i.Value > 20)
+                {
+                    Console.WriteLine($"\t{i.Key}: {i.Value}");
+                }
+            }
+        }
         static void Main(string[] args)
         {
-            var m = new Meccs("7up Joventut", "Adecco Estudiantes", 81, 73, "Palacio Mun. De Deportes De Badalona","2005-04-03");
-            Console.WriteLine($"{m.Hazai}--{m.Idegen} ({m.HPont}--{m.IPont})");
+            MasodikFeladat();
+            HarmadikFeladat();
+            NegyedikFeladat();
+            OtodikFeladat();
+            Console.WriteLine("6.feladat:");
+            HatodikFeladat();
+            Console.WriteLine("7.feladat:");
+            HetedikFeladat();
             Console.ReadLine();
         }
     }
